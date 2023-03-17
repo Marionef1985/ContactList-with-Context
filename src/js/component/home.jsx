f2b1cabede5a01c8ac34e3af664682036ac88e5a
Marione Fiszman <marionef1985@gmail.com>
2023-03-17 12:06:33 +0000

    working code, need refactorization

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { createContext, useState } from "react";
+import { useContext } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+const Contexto = createContext([]); /*Aqui Creamos el Contexto */
+
+const List = () => {
+  const users = useContext(Contexto); /*Aqui Usamos el Contexto */
+  return (
+    <>
+      {users.map((user) => (
+        <p>{user.username}</p>
+      ))}
+    </>
+  );
+};
+
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [state, setState] = useState([]);
+
+  const getData = async () => {
+    const httpResponse = await fetch(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    const users = await httpResponse.json();
+    setState(users);
+  };
+
+  return (
+    /*Aqui Damos Valor al Contexto */
+    <>
+      <Contexto.Provider value={state}>
+        <button onClick={() => getData()}>Click Me!</button>
+        <List />
+      </Contexto.Provider>
+    </>
+  );
 };
 
 export default Home;
